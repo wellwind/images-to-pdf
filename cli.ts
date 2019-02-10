da2ea7203f3d02b61d1ef175d208bbb386d3b2dd
wellwind <wellwind@mail2000.com.tw>
2019-02-10 17:05:38 +0800

    Init commit

---
+++ cli.ts
@@ -0,0 +1,3 @@
+#!/usr/bin/env node
+
+console.log('Images To PDF CLI');
---
+++ index.ts
@@ -0,0 +1,27 @@
+import * as fs from 'fs';
+import sizeOf from 'image-size';
+import PDFKit from 'pdfkit';
+
+export class ImagesToPDF {
+  convertFolderToPDF(folder: string, outputPath: string): void {
+    let doc = new PDFKit();
+    fs.readdir(folder, (_, files) => {
+      files.forEach((file, index) => {
+        const filePath = `${folder}/${file}`;
+        const size = sizeOf(filePath);
+
+        if (index === 0) {
+          doc = new PDFKit({
+            size: [size.width, size.height]
+          });
+        } else {
+          doc.addPage({ size: [size.width, size.height] });
+        }
+
+        doc.image(filePath, 0, 0, { width: size.width, height: size.height });
+      });
+      doc.pipe(fs.createWriteStream(outputPath));
+      doc.end();
+    });
+  }
+}
